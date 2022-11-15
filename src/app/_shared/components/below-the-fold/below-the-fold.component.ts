da560edd936cf025fece12192efff06db39f9392
Kyle Kemp <kyle@seiyria.com>
2022-11-15 11:47:28 -0600

    closes #38

--- src/app/_shared/components/below-the-fold/below-the-fold.component.ts
+++ src/app/_shared/components/below-the-fold/below-the-fold.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalStorage } from 'ngx-webstorage';
 
 @Component({
   selector: 'app-below-the-fold',
@@ -7,8 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BelowTheFoldComponent implements OnInit {
 
+  @LocalStorage() visualMode = '';
+
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    if(!this.visualMode) {
+      const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+      this.visualMode = prefersDark.matches ? 'dark' : 'light';
+    }
+
+    this.ensureThemeSet();
+  }
+
+  toggleMode() {
+    this.visualMode = this.visualMode === 'light' ? 'dark' : 'light';
+    this.ensureThemeSet();
+  }
+
+  ensureThemeSet() {
+    const body = document.querySelector('body');
+    body.classList.remove('dark', 'light');
+    body.classList.add(this.visualMode);
+  }
 
 }
